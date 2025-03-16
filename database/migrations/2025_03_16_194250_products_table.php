dcd25cc69059a3fa0d6cffbc05d0b5a487420d9d
Maartje Kilsdonk <maartjekilsdonk@gmail.com>
2025-03-16 21:12:21 +0100

    maker_id instead of user_id

--- database/migrations/2025_03_16_194250_products_table.php
+++ database/migrations/2025_03_16_194250_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('maker_id')->constrained('users')->cascadeOnDelete();
             $table->string('name');
             $table->text('description');
             $table->enum('type', ['Sieraden', 'Keramiek', 'Textiel', 'Kunst']); // Bijvoorbeeld: Sieraden, Keramiek, Textiel, Kunst
